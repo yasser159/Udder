d44f7143544e008356148c1da86e549e9da8e157
Yasser Hajlaoui <yasser159@gmail.com>
2021-10-07 17:37:28 -0700

    Added viewed disapear function

--- Udder/ViewController.swift
+++ Udder/ViewController.swift
@@ -17,5 +17,11 @@ class ViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         //
     }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        <#code#>
+    }
+    
+    
 }
 
